a39ca9447039edd4dd9082cf54508d137537385a
张立夫 <zlf15310382840@163.com>
2018-11-13 18:48:05 +0800

    fweff
    Signed-off-by: 张立夫 <zlf15310382840@163.com>

---
+++ src/FirstSample.java
@@ -0,0 +1,12 @@
+public class FirstSample
+    blic
+
+        static void main(String[] args) {
+
+        }
+}echo"# Java12">>README.md
+        git init
+        git add README.md
+        git commit-m"first commit"
+        git remote add origin https://github.com/ZLFSACCOUNT/Java12.git
+        git push-u origin master
